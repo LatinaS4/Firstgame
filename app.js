dfb8f0d32f4e51d99bafe84b10ab3147cb3a7626
Latina Stephens <latinastephens@latinas-mbp.home>
2020-11-05 12:18:51 -0500

    updated at 8:59 am

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
-class Deck {
-    constructor (){
-        this.deck = [];
+/* experimenting with a deck and shuffle will build out today.
+function card(value, name, suit){
+    this.value = value;
+    this.name = name;
+    this.suit = suit;
+}
+function deck(){
+    this.names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    this.suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
+    let cards = [];
 
-        const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
-        const values = ['Ace',2,3,4,5,6,7,8,9,10, 'Jacks', 'Queen', 'King'];
-
-        for (let suit in suits) {
-            for (let value in values) {
-                this.deck.push('${values[value]} of ${suits[suit]}');
-            }
+    for( let s = 0; s < this.suits.length; s++ ) {
+        for( let n = 0; n < this.names.length; n++ ) {
+            cards.push( new card ( n+1, this.names[n], this.suits[s]));
         }
+    }
+
+    return cards;
+}
+
+let myDeck = new deck();
+console.log(myDeck);
+
+window.onload = function() {
+    for(let i=0; i < myDeck.length; i++){
+        div = document.createElement('div');
+        div.className = 'card';
+
+        if(myDeck[i].suit == 'Diamonds'){
+            let ascii_char = '♦';
+        } else {
+            let ascii_char = '&' + myDeck[i].suit.toLowerCase() + ';';
+        }
+console.log(ascii_char);
+
+        div.innerHTML = '' + myDeck[i].name + '' + ascii_char + '';
+        document.body.appendChild(div);
     }
 }
 
-const deck1 = new Deck();
-
-console.log(deck1.deck);
-
-// shuffle(){
-//     const {deck} = this;
-//     let m = deck.length, i;
-
-//     while (m) {
-//         i = Math.floor(Math.random()) * m-- ) ;
-
-//         [deck [m], deck[i]] = [deck [i], deck [m]];
-//     }
-
-//     return this;
-// }
+Array.sort(function (a,b){return 0.5 - Math.random()}
+*/
